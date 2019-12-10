bd36fc6ca34979a87b38c41b34a9898326ef80ce
maxcodefaster <max.heichling@gmail.com>
2019-12-10 10:26:47 +0100

    Create auth service

---
+++ client/src/app/services/auth.service.ts
@@ -0,0 +1,68 @@
+import { Injectable, NgZone } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { NavController } from '@ionic/angular';
+import { UserService } from './user.service';
+import { DataService } from './data.service';
+import { SERVER_ADDRESS } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient,
+    private userService: UserService,
+    private dataService: DataService,
+    private navCtrl: NavController,
+    private zone: NgZone
+  ) {
+
+  }
+
+  authenticate(credentials) {
+
+    return this.http.post(SERVER_ADDRESS + 'auth/login', credentials);
+
+  }
+
+  logout() {
+
+    const headers = new HttpHeaders();
+    headers.append('Authorization', 'Bearer ' + this.userService.currentUser.token + ':' + this.userService.currentUser.password);
+
+    this.http.post(SERVER_ADDRESS + 'auth/logout', {}, { headers }).subscribe((res) => { });
+
+    this.dataService.db.destroy().then((res) => {
+
+      this.dataService.db = null;
+      this.userService.saveUserData(null);
+      this.navCtrl.navigateRoot('/login');
+
+    }, (err) => {
+      console.log('could not destroy db');
+    });
+
+  }
+
+  register(details) {
+
+    return this.http.post(SERVER_ADDRESS + 'auth/register', details);
+
+  }
+
+  validateUsername(username) {
+
+    return this.http.get(SERVER_ADDRESS + 'auth/validate-username/' + username);
+
+  }
+
+  validateEmail(email) {
+
+    const encodedEmail = encodeURIComponent(email);
+
+    return this.http.get(SERVER_ADDRESS + 'auth/validate-email/' + encodedEmail);
+
+  }
+
+}
---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  public currentUser: any = false;
+
+  constructor(public storage: Storage) {
+    console.log('Hello UserProvider Provider');
+  }
+
+  saveUserData(data): void {
+    this.currentUser = data;
+    this.storage.set('gesaQSUserData', data);
+  }
+
+  getUserData(): Promise<any> {
+    return this.storage.get('gesaQSUserData');
+  }
+
+}
